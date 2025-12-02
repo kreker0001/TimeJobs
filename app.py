99b5428be6af7e798303276a35339ae630f2afa5
kreker0001 <krona154@gmail.com>
2025-12-02 21:29:50 +0500

    Теперь SECRET_KEY генерируется каждый раз по новому при запуске app.py. Теперь п*хуй если репа будет открытой.

--- app.py
+++ app.py
@@ -12,16 +12,8 @@ import os, datetime, time
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-
-def _load_secret_key():
-    env_key = os.environ.get('SECRET_KEY') or os.environ.get('FLASK_SECRET_KEY')
-    if env_key:
-        return env_key
-    return os.urandom(32)
-
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = _load_secret_key()
+app.config['SECRET_KEY'] = os.urandom(32)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(BASE_DIR, 'time_jobs.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
